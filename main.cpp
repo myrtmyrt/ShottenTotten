29b942c6ec023ebb94a299b65f893178c7ba5c7e
myrtilleknockaert <mmyrtille.knockaert@gmail.com>
2023-05-07 19:50:39 +0200

    Maj des classes pioche et cartes faites par myrtille

--- main.cpp
+++ main.cpp
@@ -1,13 +1,16 @@
 #include <iostream>
 #include "jeu.h"
 #include "carte.h"
+#include "pioche.h"
 
 using namespace std;
 
 int main(int argc, char **argv) {
     Jeu j1 = Jeu::donneInstance(1, true, 0);
-    cout << "Nombre de manches : " << j1.getNbManchesGagnantes();
-    cout << "Partie Normale : 0 ou tactique: 1 :" << j1.getmodeDeJeu();
+    cout << "Nombre de manches : " << j1.getNbManchesGagnantes()<<endl;
+    cout << "Partie Normale : 0 ou tactique: 1 :" << j1.getmodeDeJeu()<<endl;
+    auto p = new Pioche();
+    cout<<p->getNbcarte()<<endl;
     return 0;
 }
 
--- pioche.h
+++ pioche.h
@@ -6,32 +6,32 @@
 #define PROJET_PIOCHE_H
 
 #include <iostream>
-#include <list>
+#include <vector>
 #include "carte.h"
 
 class Pioche{
 private:
-    const ::size_t nbCartesMax=54;
-    list <CarteClan> Cartes;
+    const size_t nbCartesMax=54;
+    vector<CarteClan*> Cartes;
 public:
     //constructeur
     Pioche(){
-        Couleur CouleursPossibles[]={Couleur::rouge, Couleur::bleu, Couleur::vert, Couleur::violet,Couleur::orange, Couleur::jaune};
-        Nombre NombresPossibles[]={Nombre::un,Nombre::deux,Nombre::trois,Nombre::quatre,Nombre::cinq,Nombre::six,Nombre::sept,Nombre::huit,Nombre::neuf};
-        for (int i = 0; i < sizeof(CouleursPossibles); i++) {
-            for (int j = 0; j < sizeof(NombresPossibles); j++) {
-                CarteClan newCarte(CouleursPossibles[i],NombresPossibles[j]);
-                Cartes.insert(getNbcarte()+1,newCarte);
+        Couleur CouleursPossibles[6]={Couleur::rouge, Couleur::bleu, Couleur::vert, Couleur::violet,Couleur::orange, Couleur::jaune};
+        Nombre NombresPossibles[9]={Nombre::un,Nombre::deux,Nombre::trois,Nombre::quatre,Nombre::cinq,Nombre::six,Nombre::sept,Nombre::huit,Nombre::neuf};
+        for (int i = 0; i < 6; i++) {
+            for (int j = 0; j < 9; j++) {
+                auto* newCarte = new CarteClan(CouleursPossibles[i],NombresPossibles[j]);
+                Cartes.push_back(newCarte);
             }
         }
     }
     //Accesseurs en lecture
-    ::size_t getNbcarte() const { return Cartes.size();}
-    ::size_t getNbMax()const { return nbCartesMax;}
+    size_t getNbcarte() const { return Cartes.size();}
+    size_t getNbMax()const { return nbCartesMax;}
     //méthodes
     bool estVide() const { return Cartes.size() == 0; }
     void melanger();
-    Carte piocher() const {return ::rand(Cartes);};
+//    Carte piocher() const {return rand(Cartes);};
 };
 
 class PiocheTactique{
