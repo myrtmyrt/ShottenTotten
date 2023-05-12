f1eb9121ef8424bffa7d7bc926abb151b353e976
Yushi Han <yushi.han@etu.utc.fr>
2023-05-12 21:45:58 +0000

    modification classe Borne

--- borne.cpp
+++ borne.cpp
@@ -1,43 +1,66 @@
-#include "borne.h"
+ï»¿#include "borne.h"
 
-void Borne::retirer1(const Joueur_Pioche::Carte& c) {
-	// On cherche la carte correspondante
-	size_t i = 0;
-	while (i < nb_carte1 && liste_carte1[i] != &c) i++;
-	if (i == nb_carte1) throw "Carte inexistante";
-	// On fait le d¨¦calage pour supprimer la carte
-	i++;
-	while (i < nb_carte1) {
-		liste_carte1[i - 1] = liste_carte1[i];
-		i++;
+void Borne::retirer(const Carte& c) {
+
+	int joueur1;
+	cout << "Enregistrer votre numÃ©ro pour jouer (1 ou 2)" << endl;
+	cin >> joueur1;
+
+	while ((joueur1 != 1) && (joueur1 != 2)) {
+		cout << "Votre enregistrement n'est pas valable, enregistrez 1 ou 2 s'il vous plaÃ®t" << endl;
+		cin >> joueur1;
 	}
-	nb_carte1--;
-}
 
-void Borne::retirer2(const Joueur_Pioche::Carte& c) {
-	// On cherche la carte correspondante
 	size_t i = 0;
-	while (i < nb_carte2 && liste_carte2[i] != &c) i++;
-	if (i == nb_carte2) throw "Carte inexistante";
-	// On fait le d¨¦calage pour supprimer la carte
-	i++;
-	while (i < nb_carte2) {
-		liste_carte2[i - 1] = liste_carte2[i];
+	if (joueur1 == 1) {
+		// On cherche la carte correspondante
+		while (i < nb_carte1 && liste_carte1[i] != &c) i++;
+		if (i == nb_carte1) throw "Carte inexistante";
+		// On fait le dÃ©calage pour supprimer la carte
 		i++;
+		while (i < nb_carte1) {
+			liste_carte1[i - 1] = liste_carte1[i];
+			i++;
+		}
+		nb_carte1--;
 	}
-	nb_carte2--;
+	else if (joueur1 == 2) 
+		// On cherche la carte correspondante
+		while (i < nb_carte2 && liste_carte2[i] != &c) i++;
+		if (i == nb_carte2) throw "Carte inexistante";
+		// On fait le dÃ©calage pour supprimer la carte
+		i++;
+		while (i < nb_carte2) {
+			liste_carte2[i - 1] = liste_carte2[i];
+			i++;
+		}
+		nb_carte2--;
 }
+		
 
-void Borne::ajouter1(const Joueur_Pioche::Carte& c) {
-	// Si le tableau est rempli,on ne pourra plus ajouter une carte 
-	if (nb_carte1 == nb_max) throw "Attention : nombre max atteint, vous ne pouvez pas ajouter une carte.";
+
+void Borne::ajouter(const Carte& c) {
+
+	//choix de joueur
+	int joueur2;
+	cout << "Enregistrer votre numÃ©ro pour jouer (1 ou 2)" << endl;
+	cin >> joueur2;
+
+	while (joueur2 != 1 && joueur2 != 2) {
+		cout << "Votre enregistrement n'est pas valable, enregistrez 1 ou 2 s'il vous plaÃ®t" << endl;
+		cin >> joueur2;
+	}
+
+	if (joueur2 == 1) {
+		// Si le tableau est rempli,on ne pourra plus ajouter une carte 
+		if (nb_carte1 == nb_max) throw "Attention : nombre max atteint, vous ne pouvez pas ajouter une carte.";
 		//sinon
-	const Joueur_Pioche::Carte** newtab = new const Joueur_Pioche::Carte* [nb_carte1+1];
-	// On recopie les valeurs dans le nouveau tableau
-	for (size_t i = 0; i < nb_carte1; i++) newtab[i] = liste_carte1[i];
-	// Le nouveau tableau devient le tableau du Plateau
+		const Carte** newtab = new const Carte* [nb_carte1+1];
+		// On recopie les valeurs dans le nouveau tableau
+		for (size_t i = 0; i < nb_carte1; i++) newtab[i] = liste_carte1[i];
+		// Le nouveau tableau devient le tableau du Plateau
 
-		// On pense ¨¤ d¨¦sallouer l'ancien tableau
+		// On pense Ã  dÃ©sallouer l'ancien tableau
 		
 		auto old = liste_carte1;
 		liste_carte1 = newtab;
@@ -48,25 +71,31 @@ void Borne::ajouter1(const Joueur_Pioche::Carte& c) {
 
 		nb_carte1++;
 	}
-	
 
-void Borne::ajouter2(const Joueur_Pioche::Carte& c) {
-	// Si le tableau est rempli,on ne pourra plus ajouter une carte 
-	if (nb_carte2 == nb_max) throw "Attention : nombre max atteint, vous ne pouvez pas ajouter une carte.";
-	//sinon
-	const Joueur_Pioche::Carte** newtab = new const Joueur_Pioche::Carte * [nb_carte2 + 1];
-	// On recopie les valeurs dans le nouveau tableau
-	for (size_t i = 0; i < nb_carte2; i++) newtab[i] = liste_carte2[i];
-	// Le nouveau tableau devient le tableau du Plateau
+	if (joueur2 == 2) {
+		// Si le tableau est rempli,on ne pourra plus ajouter une carte 
+		if (nb_carte2 == nb_max) throw "Attention : nombre max atteint, vous ne pouvez pas ajouter une carte.";
+		//sinon
+		const Carte** newtab = new const Carte * [nb_carte2 + 1];
+		// On recopie les valeurs dans le nouveau tableau
+		for (size_t i = 0; i < nb_carte2; i++) newtab[i] = liste_carte2[i];
+		// Le nouveau tableau devient le tableau du Plateau
 
-		// On pense ¨¤ d¨¦sallouer l'ancien tableau
+			// On pense Ã  dÃ©sallouer l'ancien tableau
 
-	auto old = liste_carte2;
-	liste_carte2 = newtab;
-	delete[] old;
+		auto old = liste_carte2;
+		liste_carte2 = newtab;
+		delete[] old;
 
-	// On ajoute la carte
-	liste_carte2[nb_carte2++] = &c;
+		// On ajoute la carte
+		liste_carte2[nb_carte2++] = &c;
 
-	nb_carte2++;
+		nb_carte2++;
+
+	}
+
 }
+
+
+	
+//Erreur de type Borne
--- borne.h
+++ borne.h
@@ -1,37 +1,61 @@
 #pragma once
-#include "Joueur_Pioche.h"
+#include "Carte.h"
 #include <iostream>
 #include <string>
 
 using namespace std;
 
-enum class Nombre { Un = 1, Deux = 2, Trois = 3, Quatre = 4, Cinq = 5, Six = 6, Sept = 7, Huit = 8, Neuf = 9 };
+
+class ErreurBorne {
+public:
+	ErreurBorne(const string& i) :info(i) {}
+	string getErreurBorne() const { return info; }
+private:
+	string info;
+};
 
 class Borne {
 private:
 	Nombre numero;
 	string image;
-	const Joueur_Pioche::Carte** liste_carte1 = nullptr;
-	const Joueur_Pioche::Carte** liste_carte2 = nullptr;
-	int gagnant;
-	int nb_carte1;
-	int nb_carte2;
-	//bool CombatBoue;
-	int nb_max = 4;
+	const Carte** liste_carte1 = nullptr;
+	const Carte** liste_carte2 = nullptr;
+	int gagnant = -1;
+	int nb_carte1 = 0;
+	int nb_carte2 = 0;
+	bool CombatDeBoue = 0;
+	int nb_max = 3;
 
 public:
 	Nombre getNumero() const { return numero; }
+	string getImage() const { return image; }
 	int getGagnant() const { return gagnant; }
+	int getNbCarte1() const { return nb_carte1; }
+	int getNbCarte2() const { return nb_carte2; }
+	int getCombatDeBoue() const { return CombatDeBoue; }
+	int getNbMax() const { return nb_max; }
+
 	//On pourrait avoir 0 cartes au d¨¦but
-	Borne() = default;
+	Borne(Nombre num, string img) : numero(num),image(img){}
 	~Borne() {
 		delete[] liste_carte1;
 		delete[] liste_carte2;
 	}
 	Borne(const Borne&) = delete;
 	Borne& operator=(const Borne& p) = delete;
-	void ajouter1(const Joueur_Pioche::Carte& c);
-	void ajouter2(const Joueur_Pioche::Carte& c);
-	void retirer1(const Joueur_Pioche::Carte& c);
-	void retirer2(const Joueur_Pioche::Carte& c);
+
+
+	void modeCombatdeBout(){ 
+		CombatDeBoue = 1; 
+		nb_max = 4;
+	}
+
+	void ajouter(const Carte& c);
+	void retirer(const Carte& c);
+
+	
+	
 };
+
+string toString(Nombre v);
+ostream& operator<<(ostream& f, Nombre v);
